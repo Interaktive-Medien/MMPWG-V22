3266b7353290ad9a28783e517f50dc1cf2bfca3c
Nick Schneeberger <nick.schneeberger@icloud.com>
2022-09-12 18:45:18 +0200

    überprüfe ob e-mail bereits registriert

--- php/register.php
+++ php/register.php
@@ -11,20 +11,51 @@ $password = $_SERVER["PHP_AUTH_PW"];
 
 // echo $_SERVER['HTTP_CUSTOMHEADER'];
 
-// verschlüssle das passwort
-$password = password_hash ($password, PASSWORD_DEFAULT);
+if (existiertEmail($email)) {
 
-$stmt = $pdo->prepare("INSERT INTO user (name, email, password) VALUES (:Name, :Email, :Password)");
+    // verschlüssle das passwort
+    $password = password_hash($password, PASSWORD_DEFAULT);
 
-$success = $stmt->execute(array('Name' => $username, 'Email' => $email, 'Password' => $password));
+    $stmt = $pdo->prepare("INSERT INTO user (name, email, password) VALUES (:Name, :Email, :Password)");
 
-// falls success true bzw. 1 ist
-if ($success){
+    $success = $stmt->execute(array('Name' => $username, 'Email' => $email, 'Password' => $password));
 
-    print_r('Registrierung erfolgreich.');
+    // falls success true bzw. 1 ist
+    if ($success) {
+
+        print_r('Registrierung erfolgreich.');
+
+    } else {
+
+        print_r($success);
+    };
 
 } else {
 
-    print_r($success);
+    print_r('Fehler: Diese E-Mail Adresse wurde wurde bereits registriert.');
 
 };
+
+function existiertEmail($email)
+{
+    require('config.php');
+
+    $stmt = $pdo->prepare("
+
+    SELECT * FROM user WHERE email='$email';
+
+    ");
+
+    if ($stmt->execute()) {
+
+        $resultateArray = $stmt->fetchAll();
+
+        if (sizeof($resultateArray) == 0) {
+
+            return true;
+        } else {
+
+            return false;
+        }
+    }
+}
